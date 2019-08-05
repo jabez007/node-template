4bafe051172733d9edd9e2c4b33ac8130b7681de
Jimmy McCann <jabez.m.007@gmail.com>
2019-08-05 14:52:42 -0500

    make sure to be able to resolve typescript imports

--- src/templates/typescript/.eslintrc.js
+++ src/templates/typescript/.eslintrc.js
@@ -1,31 +1,38 @@
 module.exports = {
-    'env': {
-        'es6': true,
-        'node': true,
+    env: {
+        es6: true,
+        node: true,
     },
-    'extends': [
+    extends: [
         'airbnb-base',
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/eslint-recommended'
     ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    'plugins': [
+    plugins: [
         '@typescript-eslint',
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'module',
-        'project': './tsconfig.json',
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json',
     },
-    'rules': {
+    rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'linebreak-style': 'off',
         'indent': 'off',
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
     },
 };
